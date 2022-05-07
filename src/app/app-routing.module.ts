12892d398fc0bed3fc9917aa440e492949afb7a9
Denis Popkov <pkvdenis@gmail.com>
2022-05-07 20:45:16 +0300

    Setup routing for CategoriesComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './questions/categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'questions/categories', component : CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
