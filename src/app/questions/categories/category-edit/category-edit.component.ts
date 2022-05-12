21a258e058e0803b1aca6948d91906614849bebd
Denis Popkov <pkvdenis@gmail.com>
2022-05-12 15:14:21 +0300

    Add validation for Category name field.

--- src/app/questions/categories/category-edit/category-edit.component.ts
+++ src/app/questions/categories/category-edit/category-edit.component.ts
@@ -14,12 +14,14 @@ export class CategoryEditComponent implements OnInit {
   category: Category;
   formCategory: Category;
   message: string;
+  nameIsValid = false;
 
   constructor(private dataService: DataService,
               private router: Router) { }
 
   ngOnInit(): void {
     this.formCategory = Object.assign({}, this.category);
+    this.checkIfNameIsValid();
   }
 
   onSubmit(): void {
@@ -42,4 +44,11 @@ export class CategoryEditComponent implements OnInit {
     this.router.navigate(['questions', 'categories'], {queryParams: {id: category.id, action: 'view'}});
   }
 
+  checkIfNameIsValid(): void {
+    if (this.formCategory.name) {
+      this.nameIsValid = this.formCategory.name.trim().length > 0;
+    } else {
+      this.nameIsValid = false;
+    }
+  }
 }
